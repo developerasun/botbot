340af8fa5a9641c2e2337f9da9b6a9c44e0e5ad2
developerasun <nellow1102@gmail.com>
2026-01-02 17:05:26 +0900

    redeploy and archive

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
-docker build -t botbot-discord:latest .
-docker service rm botbot_discord
-docker stack up -c ./service.yaml botbot
+docker build -t hub-botbot:latest .
+docker service rm hub_botbot
+docker stack up -c ./service.yaml hub
+echo [LOG] waiting 5 seconds to deploy
+sleep 5
+echo [LOG] done, listing up services
 docker service ls
